4572eebf11530b7d6ef57f6fa98ed6f360f2e49f
adrian.valverde.torres.97@gmail.com <adrian.valverde.torres.97@gmail.com>
2020-04-27 14:56:16 +0200

    Update json

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { StateMachineProvider, createStore } from "little-state-machine";
 import './App.css';
 import InfoForm from './form';
-import Result from './result';
 
 createStore({
   data: {}
@@ -16,7 +15,6 @@ function App() {
     <StateMachineProvider>
       <Router>
         <Route exact path="/" component={InfoForm} />
-        <Route path="/result" component={Result} />
       </Router>
     </StateMachineProvider>
   );
